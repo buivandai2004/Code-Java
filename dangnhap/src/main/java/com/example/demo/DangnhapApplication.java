058c530dc7d74ed3661247952225df831649a9d4
buivandai2004 <buidai.kl2004@gmail.com>
2025-05-15 14:42:39 +0700

    Chương 4 Bài thực hành 3 MVC

---
+++ dangnhap/src/main/java/com/example/demo/DangnhapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangnhapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangnhapApplication.class, args);
+	}
+
+}
